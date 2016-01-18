3f4139817a110480d5c1626140549325ba1e26f1
Thom Kiesewetter <thom@kiesewetter.nl>
2016-01-18 10:27:44 +0100

    Bug fixes and layout

--- Topo/Controllers/TTSController.cs
+++ Topo/Controllers/TTSController.cs
@@ -14,7 +14,8 @@ namespace Topo.Controllers
             System.Net.WebClient webClient = new System.Net.WebClient();
             if (Request["q"] != "")
             {
-                byte[] data = webClient.DownloadData(String.Format("http://translate.google.com/translate_tts?ie=UTF-8&tl={0}&q={1}", Request["tl"], Request["q"]));
+                string url = String.Format("http://translate.google.com/translate_tts?ie=UTF-8&tl={0}&q={1}&total=1&idx=0&textlen={2}&tk=600096&client=tw-ob&prev=input&ttsspeed=1", Request["tl"], Request["q"], Request["q"].Length);
+                byte[] data = webClient.DownloadData(url);
                 return File(data, "audio/mpeg");
             }
             return null;
